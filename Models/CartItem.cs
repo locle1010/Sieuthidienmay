2642745fcdb7164211045076ef072386f9a45a04
kduydev <cokp14@outlook.com.vn>
2026-04-01 07:23:38 +0000

    feat: add order and cart entities

---
+++ Models/CartItem.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_dienmay.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ShoppingCartId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [ForeignKey("ShoppingCartId")]
+        public ShoppingCart ShoppingCart { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        
+
+    }
+}
