a1216a83837d075417197a7578f6b02254ab6582
quocphong2004 <buifhong@gmail.com>
2026-04-01 07:30:53 +0000

    feat: add user rating stock support entities

---
+++ Models/ProductRating.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_dienmay.Models
+{
+    public class ProductRating
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string UserId { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        public string? Comment { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ Models/StockEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_dienmay.Models
+{
+    public class StockEntry
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        [Required]
+        [Range(-100000, 100000, ErrorMessage = "Số lượng phải nằm trong khoảng -100000 đến 100000")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Type { get; set; } // "Import" hoặc "Export" hoặc "Adjustment"
+
+        [StringLength(500)]
+        public string? Note { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CreatedBy { get; set; } // Username của admin
+
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public int PreviousQuantity { get; set; }
+        public int NewQuantity { get; set; }
+    }
+}
